9382e2f9e113aa6f1c39478c9a013db1a661bc4d
andyo <andyo@DESKTOP-T4847VI>
2016-11-27 17:26:25 +0900

    dao 수정

--- Project1/src/com/project1/admin/DAO/StudentDAO.java
+++ Project1/src/com/project1/admin/DAO/StudentDAO.java
@@ -1,18 +1,11 @@
 package com.project1.admin.DAO;
 
 public class StudentDAO {
-//inseon22
-	
+//I'm inseonsdfsdf
+
 /* 
-void studentAdd ()	�븰�깮 �벑濡�
-void studentListAll()	�꽑�깮�쟻 �븰�깮 �젙蹂� 異쒕젰�쓣 �쐞�븳
-void studentSearch()	�븰�깮 紐⑸줉 異쒕젰
-void studentUpdate ()	�븰�깮 �젙蹂� �닔�젙
-void studentDelete ()	�븰�깮 �젙蹂� �궘�젣
-
-
-
-4시52분 2수정
+ * 
+ * 하진이
 */
 	
 	public static void studentAdd (){
