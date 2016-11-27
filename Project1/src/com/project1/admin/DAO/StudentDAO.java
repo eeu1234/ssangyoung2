3311fa1504a84f8a4680b95bc21509bbe0ad6e36
andyo <andyo@DESKTOP-T4847VI>
2016-11-27 16:19:13 +0900

    //전하진 학생관련  dto dao 11월2\일 4시 19분

---
+++ Project1/src/com/project1/admin/DAO/StudentDAO.java
@@ -0,0 +1,37 @@
+package com.project1.admin.DAO;
+
+public class StudentDAO {
+
+	
+/*
+void studentAdd ()	학생 등록
+void studentListAll()	선택적 학생 정보 출력을 위한
+void studentSearch()	학생 목록 출력
+void studentUpdate ()	학생 정보 수정
+void studentDelete ()	학생 정보 삭제
+*/
+	
+	public static void studentAdd (){
+		
+	};
+	
+	public static void studentListAll(){
+		
+	};
+	
+	public static void studentSearch(){
+		
+	};
+	
+	public static void studentUpdate(){
+		
+	};
+	
+	public static void studentDelete(){
+		
+	};
+	
+	
+	
+	
+}
