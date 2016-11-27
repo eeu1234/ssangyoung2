ad46c3e051a8632de3df61c9534d5586abd9a313
AdamNotebook <AdamNotebook@DESKTOP-J4E36MQ>
2016-11-27 18:02:19 +0900

    수정완료 도영

--- Project1/src/com/project1/admin/DAO/StudentDAO.java
+++ Project1/src/com/project1/admin/DAO/StudentDAO.java
@@ -3,12 +3,21 @@ package com.project1.admin.DAO;
 public class StudentDAO {
 //I'm inseonsdfsdf
 	
+//Sdfsdf/
+/* sdf    
+void studentAdd ()	占쎈sdfsdfdsf린占쎄문 占쎈쾻嚥∽옙
+void studentListAll()	占쎄퐨占쎄문占쎌읅 占쎈린占쎄문 占쎌젟癰귨옙 �빊�뮆�젾占쎌뱽 占쎌맄占쎈립
+void studentSearch()	占쎈린占쎄문 筌뤴뫖以� �빊�뮆�젾
+void studentUpdate ()	占쎈린占쎄문 占쎌젟癰귨옙 占쎈땾占쎌젟
+void studentDelete ()	占쎈린占쎄문 占쎌젟癰귨옙 占쎄텣占쎌xcvxcv젫
+=======
 /* 
 void studentAdd ()	�븰�깮 �벑濡�
 void studentListAll()	�꽑�깮�쟻 �븰�깮 �젙蹂� 異쒕젰�쓣 �쐞�븳
 void studentSearch()	�븰�깮 紐⑸줉 異쒕젰
 void studentUpdate ()	�븰�깮 �젙蹂� �닔�젙
 void studentDelete ()	�븰�깮 �젙蹂� �궘�젣
+>>>>>>> branch 'master' of https://github.com/eeu1234/ssangyoung2
 */
 	
 	public static void studentAdd (){
