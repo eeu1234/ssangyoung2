e4ef77320f0f3e2c969af643dd60e9a10216a8c1
AdamNotebook <AdamNotebook@DESKTOP-J4E36MQ>
2016-11-27 16:37:02 +0900

    ㄴㅇㄹㄴㅇㄹ

---
+++ Project1/src/com/project1/admin/DAO/StudentDAO.java
@@ -0,0 +1,37 @@
+package com.project1.admin.DAO;
+
+public class StudentDAO {
+//inseon22
+	
+/* 
+void studentAdd ()	�븰�깮 �벑濡�
+void studentListAll()	�꽑�깮�쟻 �븰�깮 �젙蹂� 異쒕젰�쓣 �쐞�븳
+void studentSearch()	�븰�깮 紐⑸줉 異쒕젰
+void studentUpdate ()	�븰�깮 �젙蹂� �닔�젙
+void studentDelete ()	�븰�깮 �젙蹂� �궘�젣
+*/
+	
+	public static void studentAdd (){
+		
+	};
+	
+	public static void studentListAll(){
+		
+	};
+	
+	public static void studentSearch(){
+		
+	};
+	
+	public static void studentUpdate(){
+		
+	};
+	
+	public static void studentDelete(){
+		
+	};
+	
+	
+	
+	
+}
