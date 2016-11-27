f9c896ab1602c97e29a20a865622442a96dc792f
박진우 <@DESKTOP-2H40E5O>
2016-11-27 15:20:59 +0900

    error

--- Project1/src/com/project1/DAO/FacultyDAO.java
+++ Project1/src/com/project1/DAO/FacultyDAO.java
@@ -1,11 +1,26 @@
 package com.project1.DAO;
 
+import java.beans.Statement;
+import java.sql.Connection;
+
 public class FacultyDAO {
+	
+	private Connection conn;
+	private Statement stat;
 	
 	public FacultyDAO() { //디비 불러오기
 		
-		
+	try {
+			
+			this.conn = DBUtil.open();
+//			this.stat = conn.createStatement();
+			
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
 	}
+		
+	
 	
 	
 	public void add() {
