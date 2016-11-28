f0e713a71ede69b0fbcff94a170e61e984269de3
sist <sist@sist-PC>
2016-11-28 09:03:58 +0900

    최신 DBUtil 받아라

--- Project1/src/com/project1/DAO/DBUtil.java
+++ Project1/src/com/project1/DAO/DBUtil.java
@@ -3,82 +3,78 @@ package com.project1.DAO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-//占쌥븝옙占실댐옙 JDBC 占쏙옙占쏙옙 처占쏙옙
+//�ݺ��Ǵ� JDBC ���� ó��
 public class DBUtil {
 
+   
+   private static Connection conn = null;
+   
+   public static Connection open() {
+      
+      String url = "jdbc:oracle:thin:@eeu1234.iptime.org:1521:adam";
+      String id = "jinwoo";
+      String pw = "java1234";
+      
+      try {
+         
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+         
+         conn = DriverManager.getConnection(url, id, pw);
+         
+      } catch (Exception e) {
+         System.out.println("DBUtil : " + e.toString());
+      }
+      
+      return conn;
+      
+   }
+   
+   public static void close() {
+      
+      try {
+         
+         if (conn != null && !conn.isClosed()) {
+            conn.close();
+         }
+         
+      } catch (Exception e) {
+         System.out.println("DBUtil : " + e.toString());
+      }
+      
+   }
+   
 
-
-
-//�굹�뒗�sdfsdfsdf吏꾩씠�떎. sdsfsafsafdfasdfsdfsdfsdf
-	private static Connection conn = null;
-	
-	public static Connection open() {
-		
-		String url = "jdbc:oracle:thin:@eeu1234.iptime.org:1521:adam";
-		String id = "total";
-		String pw = "java1234";
-		
-		try {
-			 
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			
-			conn = DriverManager.getConnection(url, id, pw);
-			
-		} catch (Exception e) {
-			System.out.println("DBUtil : " + e.toString());
-		}
-		
-		return conn;
-		
-	}
-	
-	public static void close() {
-		
-		try {
-			
-			if (conn != null && !conn.isClosed()) {
-				conn.close();
-			}
-			
-		} catch (Exception e) {
-			System.out.println("DBUtil : " + e.toString());
-		}
-		
-	}
-	
-
-	
-	public static Connection open(String server, String id, String pw) {
-		
-		String url = "jdbc:oracle:thin:@" + server + ":1521:adam";
-		
-		try {
-			
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			
-			conn = DriverManager.getConnection(url, id, pw);
-			
-		} catch (Exception e) {
-			System.out.println("DBUtil : " + e.toString());
-		}
-		System.out.println();
-		return conn;
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+   
+   public static Connection open(String server, String id, String pw) {
+      
+      String url = "jdbc:oracle:thin:@" + server + ":1521:adam";
+      
+      try {
+         
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+         
+         conn = DriverManager.getConnection(url, id, pw);
+         
+      } catch (Exception e) {
+         System.out.println("DBUtil : " + e.toString());
+      }
+      
+      return conn;
+      
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 }
-
