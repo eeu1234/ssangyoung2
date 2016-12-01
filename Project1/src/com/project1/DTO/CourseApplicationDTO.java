9256a7b347501c1f3c6c1dc66b29633203ea646b
sist <sist@sist-PC>
2016-12-01 11:01:08 +0900

    g

---
+++ Project1/src/com/project1/DTO/CourseApplicationDTO.java
@@ -0,0 +1,25 @@
+package com.project1.DTO;
+
+public class CourseApplicationDTO {
+	private String lectureCode;
+	private String curriculumCode;
+	private String studentNumber;
+	public String getLectureCode() {
+		return lectureCode;
+	}
+	public void setLectureCode(String lectureCode) {
+		this.lectureCode = lectureCode;
+	}
+	public String getCurriculumCode() {
+		return curriculumCode;
+	}
+	public void setCurriculumCode(String curriculumCode) {
+		this.curriculumCode = curriculumCode;
+	}
+	public String getStudentNumber() {
+		return studentNumber;
+	}
+	public void setStudentNumber(String studentNumber) {
+		this.studentNumber = studentNumber;
+	}
+}
---
+++ Project1/src/com/project1/DTO/myCourseApplicationDTO.java
@@ -0,0 +1,41 @@
+package com.project1.DTO;
+
+public class myCourseApplicationDTO {
+	private String subjectName;
+	private String staffName;
+	private String subjectScore;
+	private String periodNum;
+	private String dayCode;
+	public String getSubjectName() {
+		return subjectName;
+	}
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+	public String getStaffName() {
+		return staffName;
+	}
+	public void setStaffName(String staffName) {
+		this.staffName = staffName;
+	}
+	public String getSubjectScore() {
+		return subjectScore;
+	}
+	public void setSubjectScore(String subjectScore) {
+		this.subjectScore = subjectScore;
+	}
+	public String getPeriodNum() {
+		return periodNum;
+	}
+	public void setPeriodNum(String periodNum) {
+		this.periodNum = periodNum;
+	}
+	public String getDayCode() {
+		return dayCode;
+	}
+	public void setDayCode(String dayCode) {
+		this.dayCode = dayCode;
+	}
+
+	
+}
